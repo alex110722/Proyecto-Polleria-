58f67c52cc79b407b28ed8ec1e27e9f93234b798
alex110722 <chavezchaconalex3b@gmail.com>
2024-06-19 01:10:18 -0500

    Create Proyecto_Polleria.cpp

---
+++ Proyecto_Polleria.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string.h>
+#include<locale.h>
+
+void 
+
+struct nacimiento{
+	int dia;
+	int mes;
+	int anio;	
+};
+
+struct cliente{
+	char[30] nombre_Cliente;
+	char[10] documento_Cliente;
+	nacimiento fecha_Cliente;
+	int puntos;	
+	int consumo;
+};
+
+struct trabajador{
+	char[30] nombre_Trabajador;
+	char[10] documento_Trabajador;	
+	nacimiento fecha_Trabajador;	
+	char[20] user;
+	char[8] contraseña;
+	char[15] cargo;	
+};
+
+struct stock{
+	float 
+	float
+	float
+	
+	
+};
+
+int main(){
+	char[10] identificacion;
+	bool error=false;
+	do{
+	cout<<"¿Es trabajador o cliente?"<<endl;
+	gets(identificacion);	       
+	}while(error);
+	
+	
+	
+		
+	return 0;
+}
