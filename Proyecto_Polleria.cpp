d301aa9991d9c6a00611c1a6363e5acb3fade7f2
alex110722 <chavezchaconalex3b@gmail.com>
2024-06-19 20:26:27 -0500

    Update Proyecto_Polleria.cpp

--- Proyecto_Polleria.cpp
+++ Proyecto_Polleria.cpp
@@ -2,6 +2,8 @@
 #include<string.h>
 #include<locale.h>
 
+using namespace std;
+
 void interfazCliente();
 void interfazTrabajador();
 
@@ -23,7 +25,7 @@ cliente9,
 cliente10;
 
 struct menu{
-    char descripcion[20];
+    char descripcion[25];
     float precio;  
 }
 plato1={"1 pollo",47.00},
@@ -34,14 +36,14 @@ plato5={"porción de papas",15.00},
 plato6={"1/2 porción de papas",10.00},
 plato7={"ensalada",10.00},
 bebida1={"gaseosa 3L",13.00},
-bebida2={"limonada 1L",15.00},
+bebida2={"limonada frozen 1L",15.00},
 bebida3={"chicha 1L",15.00};
 
 struct trabajador{
 	char nombre_Trabajador[30];
 	char documento_Trabajador[10];		
 	char user[20];
-	char contraseña[4];
+	char contrasena[6];
 	char cargo[15];	
 }
 trabajador1={"Pepe Quintana","32356489","pepequin","6489","gerente"},
@@ -51,17 +53,26 @@ trabajador4={"Gerardo Carrillo","71556889","gerardoca","6889","mozo"},
 trabajador5={"Mariana Mendoza","23132565","marianamen","2565","mozo"};
 
 struct stock{
-	float 
-	float
-	float	
-};
+	char nomProducto[15];
+	float cantidad;
+}
+producto1={"pollo",40},
+producto2={"papa",30000},
+producto3={"lechuga",20},
+producto4={"tomate",30},
+producto5={"gaseosa",25},
+producto6={"limón",20000},
+producto7={"maíz morado",20000},
+producto8={"azúcar",10000};
 
 int main(){
+	setlocale(LC_ALL, "");
 	char identificacion[10];
 	bool error=false;
 	cliente clientes[10] = {cliente1,cliente2,cliente3,cliente4,cliente5,cliente6,cliente7,cliente8,cliente9,cliente10};
 	menu carta[10] = {plato1,plato2,plato3,plato4,plato5,plato6,plato7,bebida1,bebida2,bebida3}; 
 	trabajador trabajadores[5] = {trabajador1,trabajador2,trabajador3,trabajador4,trabajador5};
+	stock productos[9] = {producto1,producto2,producto3,producto4,producto5,producto6,producto7,producto8};
 	
 	do{
 	cout<<"¿Es trabajador o cliente?"<<endl;
@@ -99,6 +110,7 @@ void interfazTrabajador(){
 	
 	
 }
+
 
 
 
