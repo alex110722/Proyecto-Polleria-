5ee610d9f1b2f7f6bc95229a324fa3150d39b482
Miguel41844x <miguel.sanchezo@unmsm.edu.pe>
2024-06-29 14:10:26 -0500

    Update Proyecto_PolleriaV2 - Miguel.cpp

--- Proyecto_PolleriaV2 - Miguel.cpp
+++ Proyecto_PolleriaV2 - Miguel.cpp
@@ -1,9 +1,3 @@
-
-
-//Todavia esta incompleta la parte de inciar sesion
-
-
-
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -70,6 +64,7 @@ struct stock {
   producto7 = {"maíz morado", 20000},
   producto8 = {"azúcar", 10000};
 
+//Interfaz para seleccionar si es cliente o trabajador
 int main() {
     setlocale(LC_CTYPE, "Spanish");
     char identificacion[10];
@@ -80,7 +75,6 @@ int main() {
     cout << "Ingrese su identificación o Salir para finalizar el programa" << endl;
     cout << "- Cliente" << endl;
     cout << "- Trabajador" << endl;
-
     do {
         gets(identificacion);
         for (int i = 0; i < 10; i++) {
@@ -90,11 +84,13 @@ int main() {
         if (strcmp(identificacion, "CLIENTE") == 0) {
             system("cls");
             interfazCliente();
+            error = false;
         } else if (strcmp(identificacion, "TRABAJADOR") == 0) {
             interfazTrabajador();
+            error = false;
         } else if (strcmp(identificacion, "SALIR") == 0) {
             cout << "Gracias por su preferencia." << endl;
-            return 0;
+            error = false;
         } else {
             error = true;
             cout << "\nIdentificación no válida. Inténtelo de nuevo: \n";
@@ -104,85 +100,98 @@ int main() {
     return 0;
 }
 
+//Interfaz para el inicio de sesión o el registro del cliente
 void interfazCliente() {
     setlocale(LC_CTYPE, "Spanish");
     int opciones;
     bool error = false;
-    do {
         cout << "Seleccione una opción: " << endl;
         cout << "1. Iniciar Sesión" << endl;
         cout << "2. Registrarse" << endl;
         cout << "3. Salir" << endl;
         do {
             cin >> opciones;
-
             if (opciones == 1) {
             	system("cls");
             	sesionUsuario();
+            	error = false;
             } else if (opciones == 2) {
                 system("cls");
                 registroUsuario();
                 system("cls");
+                error = false;
             } else if (opciones == 3) {
                 cout << "Gracias por su preferencia." << endl;
+                error = false;
             } else {
                 error = true;
                 cout << "\nOpción no válida. Inténtelo de nuevo: \n";
             }
         } while (error);
-    } while (opciones != 3);
 }
 
-void sesionUsuario(){
-	cliente y;
-	int h=0;
-	string us;
-	char usc[11];
-	char con[9];
-	ifstream sm("Datos_de_los_clientes.txt",ios_base::in);
-	if (!sm.is_open()) {
+//Interfaz para el inicio de sesión
+void sesionUsuario() {
+    cliente y;
+    int h = 0;
+    string us;
+    char usc[11];
+    char con[9];
+    bool correcto = false;
+    ifstream sm("Datos_de_los_clientes.txt", ios_base::in);
+    if (!sm.is_open()) {
         cerr << "Error al abrir el archivo." << endl;
         return;
     }
     cin.ignore();
-    while (h==0){
-	    cout << "Ingrese su usuario: ";
-	    gets(y.usuario_Cliente);
-	    bool usuarioValido = comprobarcarac(y.usuario_Cliente, 10);
-        while (!usuarioValido) {
-            cout << "\nUsuario no válido (solo caracteres alfanuméricos, _ y - hasta 10 caracteres). Inténtelo de nuevo: \n";
+    while (!correcto) {
+        h = 0; // Reiniciar el indicador de usuario encontrado
+        cout << "Ingrese su usuario (10 caracteres): ";
+        gets(y.usuario_Cliente);
+        while (comprobarcarac(y.usuario_Cliente, 10) == false) {
+            cout << "\nUsuario no válido (solo 10 caracteres, no espacios ni el caracter | ). Inténtelo de nuevo: \n";
             gets(y.usuario_Cliente);
-            usuarioValido = comprobarcarac(y.usuario_Cliente, 10);
         }
-		while(!sm.eof() && h==0){
-			getline(sm,us,'|');
-			strcpy(usc, us.c_str());
-			if (strcmp(usc, y.usuario_Cliente) == 0){
-				h++;
-			}
-			puts(usc);
-		}
-		if ( h == 0){
-			cout << "No existe el usuario";	
-			cout << "\n";
-			sm.clear();
-			sm.seekg(0);
-			}
-	}
-	cout << "Ingrese su contraseña: ";
-	gets(y.contrasena_Cliente);
-	while (comprobarcarac(y.contrasena_Cliente, 8) == false) {
-    cout << "\nContraseña no válida (8 caracteres). Inténtelo de nuevo: \n";
-    gets(y.contrasena_Cliente);
-    cout << "\n";
-	}
-	getline(sm,us,'|');
-			strcpy(usc, us.c_str());
-			if (strcmp(usc, y.usuario_Cliente) == 0){
-				h++;
-			}
+        sm.clear();
+        sm.seekg(0);
+        while (!sm.eof() && h == 0) {
+            getline(sm, us, '|');
+            strcpy(usc, us.c_str());
+            if (strcmp(usc, y.usuario_Cliente) == 0) {
+                h++;
+                getline(sm, us, '|'); // Leer la contraseña
+                strcpy(con, us.c_str()); // Almacenar la contraseña leída
+                getline(sm, us); // Leer el resto de la línea
+            } else {
+                getline(sm, us); // Leer el resto de la línea si el usuario no coincide
+            }
+        }
+        if (h == 0) {
+            cout << "No existe el usuario\n";
+            continue;
+        }
+        
+        cout << "Ingrese su contraseña: ";
+        gets(y.contrasena_Cliente);
+        while (comprobarcarac(y.contrasena_Cliente, 8) == false) {
+            cout << "\nContraseña no válida (solo 8 caracteres, no espacios ni el caracter | ). Inténtelo de nuevo: \n";
+            gets(y.contrasena_Cliente);
+            cout << "\n";
+        }
+        
+        if (strcmp(con, y.contrasena_Cliente) == 0) {
+            cout << "Inicio de sesión exitoso.\n";
+            correcto = true;
+        } else {
+            cout << "Contraseña incorrecta.\n";
+            correcto = false;
+        }
+    }
+    sm.close();
 }
-void registroUsuario(){
+
+//Interfaz para el inicio de sesion
+void registroUsuario() {
     setlocale(LC_CTYPE, "Spanish");
     ofstream es("Datos_de_los_clientes.txt", ios_base::app);
     if (!es.is_open()) {
@@ -194,8 +203,8 @@ void registroUsuario(){
         cout << "Error al abrir el archivo." << endl;
         return;
     }
-    cliente x; 
-    int r = 0, i = 0, h = 1;
+    cliente x;
+    int r = 0, i = 0, h;
     x.consumo = 0;
     x.puntos = 0;
     string us;
@@ -203,63 +212,59 @@ void registroUsuario(){
     cin.ignore();
     gets(x.nombre_Cliente);
     while (comprobarcarac(x.nombre_Cliente, 30) == false) {
-    cout << "\nNombre no válido (No debe contener el caracter | y ser menor a 30 caracteres). Inténtelo de nuevo: \n";
-    gets(x.contrasena_Cliente);
-    cout << "\n";
-	}
+        cout << "\nNombre no válido (No debe contener el caracter | y ser menor a 30 caracteres). Inténtelo de nuevo: \n";
+        gets(x.nombre_Cliente);
+    }
     cout << "Ingrese sus apellidos: ";
     gets(x.apellidos_Cliente);
     while (comprobarcarac(x.apellidos_Cliente, 30) == false) {
-    cout << "\nNombre no válido (No debe contener el caracter | y ser menor a 30 caracteres). Inténtelo de nuevo: \n";
-    gets(x.contrasena_Cliente);
-    cout << "\n";
-	}
+        cout << "\nApellidos no válidos (No debe contener el caracter | y ser menor a 30 caracteres). Inténtelo de nuevo: \n";
+        gets(x.apellidos_Cliente);
+    }
     cout << "Ingrese su documento de DNI: ";
     gets(x.documento_Cliente);
-  	while (strlen(x.documento_Cliente) != 8 || strspn(x.documento_Cliente, "0123456789") != 8) {
+    while (strlen(x.documento_Cliente) != 8 || strspn(x.documento_Cliente, "0123456789") != 8) {
         cout << "\nDocumento no válido. Inténtelo de nuevo: \n";
         gets(x.documento_Cliente);
-        cout << "\n";
     }
-     do {
+    do {
         h = 1;
         cout << "Ingrese su usuario: ";
         gets(x.usuario_Cliente);
         while (comprobarcarac(x.usuario_Cliente, 10) == false) {
-            cout << "\nUsuario no válido (10 caracteres). Inténtelo de nuevo: \n";
+            cout << "\nUsuario no válido (solo 10 caracteres, no espacios ni el caracter | ). Inténtelo de nuevo: \n";
             gets(x.usuario_Cliente);
-            cout << "\n";
         }
+        ms.clear();
         ms.seekg(0);
         while (!ms.eof() && h == 1) {
             getline(ms, us, '|');
-            if (strcmp(us.c_str(), x.usuario_Cliente) == 0){
-            	cout<<us<<endl;
+            if (strcmp(us.c_str(), x.usuario_Cliente) == 0) {
                 h = 0;
             }
+            getline(ms, us); // Leer el resto de la línea
         }
         if (h == 0) {
-            cout << "Ya existe el usuario";
-            cout << "\n";
-            ms.clear();
-            ms.seekg(0);
+        	cout<<endl;
+            cout << "Ya existe el usuario "<<endl;
+            cout<<endl;
         }
     } while (h == 0);
-	cout << "Ingrese su contraseña: ";
-	gets(x.contrasena_Cliente);
-	while (comprobarcarac(x.contrasena_Cliente, 8) == false) {
-    cout << "\nContraseña no válida (8 caracteres). Inténtelo de nuevo: \n";
+    cout << "Ingrese su contraseña: ";
     gets(x.contrasena_Cliente);
-    cout << "\n";
-}
-	
-	cout << "Usuario Registrado"<<endl;
-	system("pause");
-    es << x.usuario_Cliente << "|" << x.contrasena_Cliente << "|" <<x.nombre_Cliente << "|" << x.apellidos_Cliente << "|" << x.documento_Cliente << "|" << x.puntos <<"|" << x.consumo << endl;
+    while (comprobarcarac(x.contrasena_Cliente, 8) == false) {
+        cout << "\nContraseña no válida (solo 8 caracteres, no espacios ni el caracter | ). Inténtelo de nuevo: \n";
+        gets(x.contrasena_Cliente);
+    }
+    
+    cout << "Usuario Registrado\n";
+    system("pause");
+    es << x.usuario_Cliente << "|" << x.contrasena_Cliente << "|" << x.nombre_Cliente << "|" << x.apellidos_Cliente << "|" << x.documento_Cliente << "|" << x.puntos << "|" << x.consumo << "|" << endl;
     es.close();
     ms.close();
 }
 
+//Función para comprobar la longitud y el uso de algunos caracteres
 bool comprobarcarac(char x[], int n) {
     int r = 0, i = 0, h = 0;
     if (n<20){
